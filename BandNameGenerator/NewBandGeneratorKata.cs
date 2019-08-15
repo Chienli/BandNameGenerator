23c8cf4cb785f134be1a4e675ed4977978fdaf68
Guy Lin <xup6ru03xu4@gmail.com>
2019-08-16 06:16:07 +0800

    Diff_first_end_return_The_Blank_Band pass

---
+++ BandNameGenerator/NewBandGeneratorKata.cs
@@ -0,0 +1,15 @@
+﻿namespace BandNameGenerator
+{
+    public class NewBandGeneratorKata
+    {
+        public string GetNewBandName(string str)
+        {
+            if (str[0] != str[str.Length - 1])
+            {
+                return $"The {str[0].ToString().ToUpper() + str.Substring(1, str.Length - 1)}";
+            }
+
+            return "";
+        }
+    }
+}
---
+++ BandNameGenerator/NewBandGeneratorKataTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BandNameGenerator
+{
+    [TestClass]
+    public class NewBandGeneratorKataTest
+    {
+        private readonly NewBandGeneratorKata _kata = new NewBandGeneratorKata();
+
+        [TestMethod]
+        public void Diff_first_end_return_The_Blank_Band()
+        {
+            var actual = _kata.GetNewBandName("soul");
+
+            Assert.AreEqual("The Soul", actual);
+        }
+    }
+}
